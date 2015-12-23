8daa6cbab07cd1e053ee4e9f9e3435ae321ddd32
panpan2 <p.panayiotou2@gmail.com>
2015-12-23 10:07:27 +0200

    Moved SetupLogic to SetupGameController and major changes in Setup function

--- Kni/Assets/Sripts/Components/GameManager/BoardController.cs
+++ Kni/Assets/Sripts/Components/GameManager/BoardController.cs
@@ -30,39 +30,5 @@ namespace Assets.Sripts.Components.GameManager
             // TODO: Initialise stone game object at the given position using the prefab field
         }
 
-		//Setup logic
-		public bool checkForSquaresContaining(int i, int j, CellState[,] board, Player player, int dimensions)
-		{
-			//post: returns if a stone of the player can be placed in cell (i, j) 
-			int totalStonesOfPlayer = 0;
-			for(int dimension = 2; dimension <= dimensions; dimension++)
-			{
-				//TODO, check squares around
-			}
-			return false;
-		}
-
-		public bool[,] PossiblePositions(CellState[,] board, Player player)
-		{
-
-			int width = board.GetLength (0);
-			int height = board.GetLength (1);
-		
-			bool[,] canBePlaced = new bool[width, height];
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-
-					if((int)board[i, j] != 2) // Empty
-					{
-						canBePlaced[i, j] = false;
-						continue;
-					}
-					
-					canBePlaced[i, j] 
-						= checkForSquaresContaining(i, j, board, player, Math.Min(width, height));
-				}
-			}
-			return canBePlaced;
-		}
     }
 }
--- Kni/Assets/Sripts/Core/Game/SetupGameController.cs
+++ Kni/Assets/Sripts/Core/Game/SetupGameController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Assets.Sripts.Core.Input;
 using Assets.Sripts.Components.UserInput;
 using Assets.Sripts.Components.GameManager;
+using Assets.Sripts.Core.Board;
 
 namespace Assets.Sripts.Core.Game
 {
@@ -29,5 +30,59 @@ namespace Assets.Sripts.Core.Game
             BoardController.Instance.Insert(args.Position, CurrentPlayer);
             throw new NotImplementedException();
         }
+
+		//Setup logic
+		public bool checkForSquaresContaining(int i, int j, List<CellCoordinates> stones)
+		{
+			//post: returns if a stone of the player can be placed in cell (i, j) 
+			//TODO
+			int totalStonesOfPlayer = 0;
+			
+			return false;
+		}
+		
+		public bool[,] PossiblePositions(CellState[,] board, Player player)
+		{
+			
+			int width = board.GetLength (0);
+			int height = board.GetLength (1);
+			
+			List<CellCoordinates> stones = new List<CellCoordinates> (); //stones of same color
+			bool[,] canBePlaced = new bool[width, height];
+			
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					
+					if((int)board[i, j] != 2) // Empty
+					{
+						canBePlaced[i, j] = false;
+						continue;
+					}
+					canBePlaced[i, j] = false;
+					if((int)board[i, j] == (int)player)
+					{
+						stones.Add(new CellCoordinates(i, j));
+					}
+					else if((int)board[i, j] == 2) //empty
+					{
+						canBePlaced[i, j] = true;
+					}
+					
+				}
+			}
+			
+			for (int i = 0; i < width; i++)
+			{
+				for(int j = 0; j < width; j++)
+				{
+					if(canBePlaced[i,j])
+						canBePlaced[i, j] 
+						= checkForSquaresContaining(i, j, stones);
+				}
+			}
+			
+			return canBePlaced;
+		}
+
     }
 }
